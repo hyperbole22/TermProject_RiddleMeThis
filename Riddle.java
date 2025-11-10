d6ab472506322633ce38c7197ff355dc537a29fe
kiley <kc17695p@pace.edu>
2025-11-10 18:29:37 -0500

    riddles

---
+++ Riddle.java
@@ -0,0 +1,50 @@
+//imports
+
+//
+
+
+
+
+//riddle one
+
+    //hint 1-1
+
+    //hint 1-2
+
+    //hint 1-3
+
+
+//riddle two
+
+    //hint 2-1
+
+    //hint 2-2
+
+    //hint 2-3
+
+
+//riddle three
+
+    //hint 3-1
+
+    //hint 3-2
+
+    //hint 3-3
+
+
+//riddle four
+
+    //hint 4-1
+
+    //hint 4-2
+
+    //hint 4-3
+
+
+//riddle five
+
+    //hint 5-1
+
+    //hint 5-2
+
+    //hint 5-3
---
+++ RiddleTests.java
@@ -0,0 +1,11 @@
+//import static org.junit.Assert.*;
+import java.io.*;
+import java.beans.*;
+
+public class RiddleTests {
+//    private Riddle riddles;
+    
+//@Before
+
+//@Test
+}
