18cdaca8e6fbe37f23baa252535d632aaefd350d
Alex <ks64698p@pace.edu>
2025-12-10 18:15:26 -0500

    p o o p y

--- Riddle.java
+++ Riddle.java
@@ -1,41 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Riddle {
 
-    public static final String rules = "welcome to Riddle Me This! \n " +
-                    "you will be given five random riddles to solve \n" +
-                    "all answers will be one word answers, other kinds of answers will not be accepted \n" +
-                    "after three guesses you will be offered a hint\n" +
-                    "there are three hints for every riddle\n" + 
-                    "solve all five riddles to win the game\n" +
-                    "if you exceed ten guesses on one riddle, you will automatically lose the game";
+    public static final String rules = """
+                                       welcome to Riddle Me This! 
+                                       you will be given five random riddles to solve 
+                                       all answers will be one word answers, other kinds of answers will not be accepted 
+                                       after three guesses you will be offered a hint
+                                       there are three hints for every riddle
+                                       solve all five riddles to win the game
+                                       if you exceed ten guesses on one riddle, you will automatically lose the game
+                                       if you are confident in your guess but it says that it's wrong, try again 
+                                       with or without an 's' (ex. boat and boats)""";
 
-    public static final String loss = "wow you suck at this... \n" +
-                  "better luck next time I guess";
+    public static final String loss = """
+                                        wow you suck at this...
+                                        better luck next time I guess""";
     
-    public static final String win = "congrats you actually managed to solve all the riddles! \n" +
-                 "too bad even a 3rd grader could do that...";
+    public static final String win = """
+                                     congrats you actually managed to solve all the riddles! 
+                                     too bad even a 3rd grader could do that...""";
 
     public static final String RESET = "\033[0m";
     public static final String YELLOW = "\033[33m";
     
     //Put all riddles in at start of program
     public static void main(String[] args) {
-        System.out.println("debug");
-        File riddleFile = new File("riddles.txt");
-        System.out.println("debug1");
-        File hintFile = new File("hints.txt");
         Scanner scanner = new Scanner(System.in);
         HashMap<String, String> riddleMap = new HashMap<>();
         HashMap<String, List<String>> hintMap = new HashMap<>();
         
         //read riddles from file and store in hashmap
         try {
-            System.out.println("debug2");
-            while (scanner.hasNextLine()) {
-                System.out.println("debug3"); // didn't print
-                String line = scanner.nextLine();
+            File riddleFile = new File("riddles.txt");
+            Scanner scannerRiddles = new Scanner(riddleFile);
+            while (scannerRiddles.hasNextLine()) {
+                String line = scannerRiddles.nextLine();
                 String[] parts = line.split(":");
                 if (parts.length == 2){
                     String answer = parts[0].trim();
@@ -43,35 +44,31 @@ public class Riddle {
                     riddleMap.put(answer, riddle);
                 }
             }
+            scannerRiddles.close();
         } 
-        catch (Exception e) {
+        catch (FileNotFoundException e) {
             System.out.println("error reading file");
-            e.printStackTrace();
         }
 
         //read hints from file and store in hashmap
         try {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+            File hintFile = new File("hints.txt");
+            Scanner scannerHints = new Scanner(hintFile);
+            while (scannerHints.hasNextLine()) {
+                String line = scannerHints.nextLine();
                 String[] parts = line.split(":");
                 if (parts.length == 2) {
                     String answer = parts[0].trim();
                     String[] hints = parts[1].split(",");
-                    if (hints.length == 3) {
-                        //idk if i need this part
-                        String hintOne = hints[0];
-                        String hintTwo = hints[1];
-                        String hintThree = hints[2];
-                    }
                     hintMap.put(answer, Arrays.asList(hints));
                 }
             }
+            scannerHints.close();
         }
         catch (Exception e) {
             System.out.println("error reading file");
             e.printStackTrace();
-        }
-        
+        }        
         
         //Create and shuffle a complete list of riddles
         ArrayList<String>shuffledRiddles = new ArrayList();
@@ -84,44 +81,66 @@ public class Riddle {
         Queue<String> riddlesToDo = new LinkedList<>();
         for (int i = 0; i < 5; i++) {
             riddlesToDo.offer(shuffledRiddles.get(i));
-        }
-
-
-    
+        }    
 
         while (!riddlesToDo.isEmpty()) {
             String currentRiddle = riddlesToDo.poll();
             boolean solved = false;
             int attempts = 0;
+            int hints = 0;
             while(!solved) {
-                System.out.println("riddle");
+                System.out.println(currentRiddle);
                 System.out.println("enter your guess: ");
                 String userGuesses = scanner.nextLine();
-                if (userGuesses.equalsIgnoreCase("riddle")) {
+                if (userGuesses.equalsIgnoreCase(riddleMap.entrySet().stream()
+                        .filter(entry -> entry.getValue().equals(currentRiddle))
+                        .map(Map.Entry::getKey)
+                        .findFirst()
+                        .orElse(""))) {
                     System.out.println("good job! you actually got one!");
                     solved = true;
                 }
                 else {
                     //add option for hints after three wrong guesses
                     attempts++;
-                    if (attempts >= 3) {
-                        System.out.println("would you like a hint? (y/n): ");
-                        String wantHint = scanner.nextLine();
-                        if (wantHint.equalsIgnoreCase("y")){
-                            System.out.println("here is your hint: ");
-                        }
-                        else if (wantHint.equalsIgnoreCase("n")){
-                            System.out.println("really? okay... try again I guess");
-                        }
-                        else {
-                                System.out.println("that wasn't either option... try again");
-                        }                       
+                    if (attempts == 10) {
+                        System.out.println(loss);
+                        System.exit(0);
                     }
                     System.out.println("wrong! how can you not get it?");
+                    
+                    if (attempts >= 3) {
+                        if (hints < 3) {
+                            System.out.println("would you like a hint? (y/n): ");
+                            String wantHint = scanner.nextLine();
+                            
+                            if (wantHint.equalsIgnoreCase("y")){
+                                System.out.println("here is your hint: ");
+                                String hint = riddleMap.entrySet().stream()
+                                        .filter(entry -> entry.getValue().equals(currentRiddle))
+                                        .map(Map.Entry::getKey)
+                                        .findFirst()
+                                        .orElse("");
+                                System.out.println(hintMap.get(hint).get(attempts - 3));
+                                hints++;
+                                
+                            }
+                            else if (wantHint.equalsIgnoreCase("n")){
+                                System.out.println("really? okay... try again I guess");
+                            }
+                            else {
+                                    System.out.println("that wasn't either option... try again");
+                            }              
+                        }
+                        else {
+                            System.out.println("you have used all your hints for this riddle, try again!");
+                        }
+                    }
                 }
             }
         }
 
+        System.out.println(win);
         System.out.println("congratulations! you have solved all the riddles!");
         System.out.println("here is your prize:");
         System.out.println(YELLOW + " .  .  .  .");
@@ -129,6 +148,10 @@ public class Riddle {
         System.out.println(YELLOW + "|          |");
         System.out.println(YELLOW + "|          |");
         System.out.println(YELLOW + "------------" + RESET);
+    }
+
+    public static String getRules() {
+        return rules;
     }
 }
 
